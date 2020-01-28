354d1268b9a957920e90e85fa31151a5ac8fdb7b
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-01-28 15:33:58 +0700

    CI hotfix

--- .test.sh
+++ .test.sh
@@ -17,9 +17,13 @@ psql -v ON_ERROR_STOP=ON -f .test_pgator_rpc_table.sql "$CONNINFO_UNQUOTED"
 ./pgator --config=${1} --debug=true &
 trap "kill %%" EXIT
 
-ADDRESS=`jq '.listenAddresses[0]' ${1}`
+# broken at ~Jan 2020 by unknown reason:
+#ADDRESS=`jq '.listenAddresses[0]' ${1}`
+#PORT=`jq '.listenPort' ${1}`
+ADDRESS="127.0.0.1"
+PORT=8080
+
 ADDRESS_UNQUOTED=`echo $ADDRESS | xargs`
-PORT=`jq '.listenPort' ${1}`
 
 # Start test client
 dub run pgator:test --build=unittest -- "$ADDRESS_UNQUOTED" "$PORT"
